c15271b6d9b0235e8eacb8bc10df5dc076eddd6e
AndriiHliuza <gl093770@gmail.com>
2023-02-17 19:14:57 +0200

    Initial commit

---
+++ src/main/java/com/itea/andrii/IteaJavaProDockerApplication.java
@@ -0,0 +1,13 @@
+package com.itea.andrii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IteaJavaProDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IteaJavaProDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itea/andrii/IteaJavaProDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itea.andrii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IteaJavaProDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
